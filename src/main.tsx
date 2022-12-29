5f5207dba6ae9b587f2f21f0fc275f3c4d800320
Beshoy S. Said <72396606+BeshoyS@users.noreply.github.com>
2022-12-29 23:19:26 +0100

    Adding dist

--- src/main.tsx
+++ src/main.tsx
@@ -18,6 +18,10 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
+        path: "rest-countries-api-ftm/",
+        element: <Home />,
+      },
+      {
         path: "countries/:name",
         element: <Details />,
       },
